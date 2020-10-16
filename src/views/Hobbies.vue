a02a72baad0423917e11baeb34c0d9456f80dd0f
PutridFarm <2357827+PutridFarm@users.noreply.github.com>
2020-10-16 10:07:11 -0400

    Removing development files from main branch
    Main branch will only be used for deployments. All development files moved to development branch.

--- src/views/Hobbies.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="hobbies">
-    <h1>Hobbies</h1>
-    <p>Coming soon</p>
-  </div>
-</template>
--- src/views/Projects.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="projects">
-    <h1>Projects</h1>
-    <p>Coming soon</p>
-  </div>
-</template>
